8777e075d13e35ebbb7b9f42544ab1b7cba7b4cd
Zdenek Ryska <zryska@osisoft.com>
2021-12-10 11:59:57 +0100

    Initial reproduction contents

---
+++ TestApp/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.Extensions.Hosting.WindowsServices;
+
+var options = new WebApplicationOptions
+{
+    Args = args,
+    ContentRootPath = WindowsServiceHelpers.IsWindowsService() ? AppContext.BaseDirectory : default
+};
+
+var builder = WebApplication.CreateBuilder(options);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Host.UseWindowsService();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ repro.ps1
@@ -0,0 +1,11 @@
+﻿$attempt = 1
+$serviceName = '.NET6 Test'
+while($true)
+{
+    Write-Host "Service restart attempt: $attempt"
+    Stop-Service -Name $serviceName
+    Start-Sleep -s 1
+    Start-Service -Name $serviceName
+    Start-Sleep -s 2
+    $attempt = $attempt + 1
+}
